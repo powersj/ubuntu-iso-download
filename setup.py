df7fcc88db578dbaaf1d55c4c86cd7d8cde2b07e
Joshua Powers <josh.powers@canonical.com>
2018-12-29 13:58:57 -0800

    Release 18.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(README_FILE, 'r') as readme:
 
 setup(
     name='ubuntu-iso-download',
-    version='18.2',
+    version='18.3',
     description='Download the latest Ubuntu ISOs.',
     long_description=README_TEXT,
     long_description_content_type='text/markdown',
